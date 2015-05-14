432c319b22af2d27f0055b754e4ab1ed002bafbd
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-05-14 15:48:56 -0700

    [rails 4 upgrade, paperclip upgrade] The file extension has to match the filetype as determined by Paperclip::ContentTypeDetector, otherwise a "spoofed_media_type" exception is thrown, which cannot be turned off. See: https://github.com/thoughtbot/paperclip#security-validations

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,9 @@
 # https://github.com/thoughtbot/paperclip/issues/1677#issuecomment-102159964
 Paperclip::UploadedFileAdapter.content_type_detector = Paperclip::ContentTypeDetector
+
+# The file extension has to match the filetype as determined by Paperclip::ContentTypeDetector,
+# otherwise a "spoofed_media_type" exception is thrown, which cannot be turned off.
+# See: https://github.com/thoughtbot/paperclip#security-validations
+Paperclip.options[:content_type_mappings] = {
+  sql: 'text/plain'
+}
