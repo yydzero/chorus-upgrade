e948b7abd07d1debb5e26e53323465dc459895a7
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-05-15 11:03:35 -0700

    [rails 4 upgrade, paperclip upgrade] Fixing bugs related to specs which were written in 2012 to double check that paperclip "validates_media_type_spoof_detection" was working.  The details of how this works have changed in the intervening time and, some tests were failing. I will wrong a longer explanation in a code comment if, this solution is something we leave in long term.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -7,3 +7,24 @@ Paperclip::UploadedFileAdapter.content_type_detector = Paperclip::ContentTypeDet
 Paperclip.options[:content_type_mappings] = {
   sql: 'text/plain'
 }
+
+module Paperclip
+  class MediaTypeSpoofDetector
+
+    def spoofed?
+
+      # KT TODO: Do not spoof check empty files.  It's complicated.  I will write a better comment if this is something
+      # we will leave in long term.
+      return if is_empty?
+
+      if has_name? && has_extension? && media_type_mismatch? && mapping_override_mismatch?
+        Paperclip.log("Content Type Spoof: Filename #{File.basename(@name)} (#{supplied_file_content_types}), content type discovered from file command: #{calculated_content_type}. See documentation to allow this combination.")
+        true
+      end
+    end
+
+    def is_empty?
+      calculated_content_type == "application/x-empty"
+    end
+  end
+end
