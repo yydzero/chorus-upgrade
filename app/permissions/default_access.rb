6695a3e9b5ab269b4c356b3d9afa39e2ee30a63c
prakash-alpine <prakash@alpinenow.com>
2015-05-25 07:42:48 -0700

    Changed delegate method to use "object.send" method to call private current_user method.

--- app/permissions/default_access.rb
+++ app/permissions/default_access.rb
@@ -3,7 +3,12 @@ require 'allowy'
 class DefaultAccess
   include Allowy::AccessControl
 
-  delegate :current_user, :to => :context
+  #delegate :current_user, :to => :context
+
+  # Prakash. User the 'send' method to call private current_user method so that it can be called by Specs.
+  def current_user
+    context.send(:current_user)
+  end
 
   def access_for(model)
     context.current_allowy.access_control_for(model) || DefaultAccess.new(context)
